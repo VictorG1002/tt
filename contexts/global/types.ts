b0a545993050def17af10c6cdb8f34f011029ba5
Victor Oliveira <vggaia10@gmail.com>
2024-04-27 17:34:36 -0300

    add interfaces

--- contexts/global/types.ts
+++ contexts/global/types.ts
@@ -8,3 +8,24 @@ export interface IGlobalContext {
 export interface IGlobalProviderProps {
   children: React.ReactNode;
 }
+export interface MyLocation {
+  name: string;
+  id: string;
+  parentId: string | null;
+}
+export interface MyAsset {
+  name: string;
+  id: string;
+  locationId: string | null;
+  parentId: string | null;
+  sensorType: string | null;
+  status: string | null;
+}
+export interface MyTreeItem {
+  id?: string;
+  parentId?: string | null;
+  children?: MyTreeItem[];
+  name?: string;
+  sensorType?: string | null;
+  status?: string | null;
+}
